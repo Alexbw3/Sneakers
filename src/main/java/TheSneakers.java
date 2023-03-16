7147778aeb4693662b2c8bf6d61d8c90aefacbea
alex lira barrios <22670046@itiguala.edu.mx>
2023-03-15 18:57:27 -0600

    Se crea proyecto Sneakers

---
+++ src/main/java/TheSneakers.java
@@ -0,0 +1,13 @@
+public class TheSneakers {
+    public static void main(String[] args) {
+        Sneakers zapatos = new Sneakers();
+        zapatos.setModel(2349899);
+        zapatos.setSize(6.5);
+        zapatos.setColor("blancos");
+        zapatos.setMarca("nike");
+
+
+        zapatos.toString();
+    }
+
+}
